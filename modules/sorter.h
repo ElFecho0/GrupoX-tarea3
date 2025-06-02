93d8ed6ede840eb67b791595ada4efc65363d62d
Matisvx <matiasasilva@alumnos.uai.cl>
2025-06-02 12:33:47 -0400

    Create sorter.h

---
+++ modules/sorter.h
@@ -0,0 +1,11 @@
+// modules/validator.c
+#include <string.h>
+#include "validator.h"
+
+// modules/sorter.h
+#ifndef SORTER_H
+#define SORTER_H
+
+void imprimirCiudadesOrdenadas(char** nombres, int cantidad);
+
+#endif
