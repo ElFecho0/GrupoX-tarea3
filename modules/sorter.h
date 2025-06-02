5d9067fb78e785de55b46869496420609e4a3b96
Matisvx <matiasasilva@alumnos.uai.cl>
2025-06-02 13:50:58 -0400

    Update sorter.h

--- modules/sorter.h
+++ modules/sorter.h
@@ -1,10 +1,1 @@
-#include <string.h>
-#include "validator.h"
 
-#ifndef SORTER_H
-#define SORTER_H
-
-// Se indica que hay una función la cual imprime las ciudades de forma ordenada
-void imprimirCiudadesOrdenadas(char** nombres, int cantidad);
-
-#endif
