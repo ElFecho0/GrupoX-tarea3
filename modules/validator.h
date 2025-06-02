00fa6883894bd9c9362fbd472963514345552780
Matisvx <matiasasilva@alumnos.uai.cl>
2025-06-02 13:05:07 -0400

    Update validator.h

--- modules/validator.h
+++ modules/validator.h
@@ -1,8 +1,10 @@
 #ifndef VALIDATOR_H
 #define VALIDATOR_H
 
+// En "processor.h" esta la definición de los grafos, necesaria para validar si las ciudades existen
 #include "processor.h"
 
+// Indica que hay una función que genera un valor el cual es 1 si la ciudad esta en el grafo y 0 su no existe
 int ciudadExiste(Grafo* grafo, const char* ciudad);
 
 #endif
